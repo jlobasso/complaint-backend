7e406cf54a93b37e463a2ecef9df03fcbdefb9b8
jlobasso <jlobasso>
2020-03-03 12:33:05 -0300

    first commit

---
+++ config/mongoose/connect.js
@@ -0,0 +1,6 @@
+//const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:3306');
+
+//mongoose.connect('mongodb://localhost/auth');
+
+//module.exports = mongoose;
---
+++ config/mysql/connect.js
@@ -0,0 +1,21 @@
+//var mysql = require('mysql');
+
+//var conexion = mysql.createConnection({
+// host: 'localhost',
+//  user: 'root',
+//  password: 'asteroide',
+//  database: 'complaint',
+//  port: 3306
+//});
+
+//conexion.connect(function (error) {
+//  if (error) {
+//    console.log(error);
+//    console.log('connection failed');
+//  }
+//  else {
+//    console.log('successful connection');
+//  }
+//});
+
+//module.exports = conexion;
